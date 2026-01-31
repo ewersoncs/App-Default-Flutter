bf657467e343a1df79ed7f9f675ce8b2063e8925
Ewerson Castelo Silva <ewerson.castelo@gmail.com>
2026-01-31 17:00:50 -0400

    chore: add background and textStyle for app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const CupertinoApp(
-      home: Text("Hello World!!!"),
+    CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 58, 12, 134),
+                Color.fromARGB(255, 88, 19, 156),
+              ],
+              begin: AlignmentGeometry.topLeft,
+              end: AlignmentGeometry.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "Hello World!!!",
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
+        ),
+      ),
     ),
   );
 }
