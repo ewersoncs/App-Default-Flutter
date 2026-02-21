5d58e3530cc2938a070c83b6cd73b9c979c4fdad
Ewerson Castelo Silva <ewerson.castelo@gmail.com>
2026-02-21 00:57:37 -0400

    chore: set variable text to widget custom

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -24,7 +24,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: StyledText(),
+        child: StyledText("Hello World"),
       ),
     );
   }
